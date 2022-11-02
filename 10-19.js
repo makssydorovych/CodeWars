7bd9e8073dae35f37e9fb94d909e944609b51dd0
Maksym <maksymsydorovych@gmail.com>
2022-11-02 20:25:37 +0100

    10

---
+++ 10-19.js
@@ -0,0 +1,14 @@
+// Given two integers a and b, which can be positive or negative, find
+// the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+//
+//     Note: a and b are not ordered!
+function GetSum(a, b) {
+    if(a == b) {
+        return a
+    }
+    else if (a < b) {
+        return a + GetSum(a+1, b)
+    } else {
+        return a + GetSum(a-1, b)
+    };
+}
